51b8ce14a8f5c85973ab2d0482048e09b428d4c6
Riccardo <ricky.pagliuca@gmail.com>
2019-03-09 19:06:30 +0100

    init

--- source/buddy_allocator.c
+++ source/buddy_allocator.c
@@ -28,26 +28,37 @@ int startIdx(int idx){
 // computes the size in bytes for the allocator
 // I use methods offered by the bitmap
 int BuddyAllocator_calcSize(int num_levels) {
-  int num_bits = 1<<(num_levels) - 1; // bit used in the bitmap
-  return num_bits;
+  int num_bits = 1<<(num_levels) - 1; //Bit used in the bitmap
+  return BitMap_getBytes(num_bits) + sizeof(BitMap);   //Bytes used in the bitmap
 }
-
 
 void BuddyAllocator_init(BuddyAllocator* alloc,
                          int num_levels,
+                         char* buffer,
+                         int buffer_size,
                          char* memory,
                          int min_bucket_size){
 
-  // we need room also for level 0
+  //num_levels includes also the zero level (e.g. if num_levels is 1 there is just the zero level)
   alloc->num_levels=num_levels;
   alloc->memory=memory;
   alloc->min_bucket_size=min_bucket_size;
-  assert (num_levels<MAX_LEVELS);
   
+  //I use the buffer to store both the BitMap struct and the BitMap buffer storing the bits
+  //For this reason the first sizeof(BitMap) bytes will be used to store the struct
+  alloc->tree = (BitMap*)buffer;
+  assert (num_levels<MAX_LEVELS);
+  // we need enough memory to handle internal structures
+  assert (buffer_size>=BuddyAllocator_calcSize(num_levels));
+  
+  int num_bits = 1<<(num_levels) - 1; 
   //init the bitmap
-  BitMap_init(alloc->bitmap,BuddyAllocator_calcSize(num_levels),
-
-  int list_items=1<<(num_levels+1); // maximum number of allocations, used to size the list
-  int list_alloc_size=(sizeof(BuddyListItem)+sizeof(int))*list_items;
+  BitMap_init(alloc->tree,num_bits,(uint8_t*)(buffer + sizeof(BitMap))); 
+  
 };
 
+void* BuddyAllocator_malloc(BuddyAllocator* alloc, int size){
+	//TO DO
+	
+	return NULL;	
+}
--- source/buddy_allocator.h
+++ source/buddy_allocator.h
@@ -5,10 +5,10 @@
 
 typedef struct  {
 	//I just need a bitmap to handle the allocator
-	BitMap tree;
+	BitMap* tree;
 	int num_levels;
 	char* memory; // the memory area to be managed
-	int min_bucket_size; // the minimum page of RAM that can be returned
+	int min_bucket_size; // the minimum size page of RAM that can be returned
 } BuddyAllocator;
 
 
@@ -23,16 +23,6 @@ void BuddyAllocator_init(BuddyAllocator* alloc,
                          int buffer_size,
                          char* memory,
                          int min_bucket_size);
-
-// returns (allocates) a buddy at a given level.
-// side effect on the internal structures
-// 0 id no memory available
-BuddyListItem* BuddyAllocator_getBuddy(BuddyAllocator* alloc, int level);
-
-
-// releases an allocated buddy, performing the necessary joins
-// side effect on the internal structures
-void BuddyAllocator_releaseBuddy(BuddyAllocator* alloc, BuddyListItem* item);
 
 //allocates memory
 void* BuddyAllocator_malloc(BuddyAllocator* alloc, int size);
